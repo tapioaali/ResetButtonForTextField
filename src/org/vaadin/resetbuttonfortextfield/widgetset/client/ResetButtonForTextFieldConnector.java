3a40603134fd25cfa4c85d51721a119d3d433293
tapio <tapio@vaadin.com>
2013-02-14 13:31:33 +0200

    Changed wrapper of the reset button to HTML

--- src/org/vaadin/resetbuttonfortextfield/widgetset/client/ResetButtonForTextFieldConnector.java
+++ src/org/vaadin/resetbuttonfortextfield/widgetset/client/ResetButtonForTextFieldConnector.java
@@ -8,7 +8,7 @@ import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.event.logical.shared.AttachEvent;
 import com.google.gwt.user.client.DOM;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HTML;
 import com.vaadin.client.ComponentConnector;
 import com.vaadin.client.ServerConnector;
 import com.vaadin.client.communication.StateChangeEvent;
@@ -22,27 +22,27 @@ public class ResetButtonForTextFieldConnector extends
         AttachEvent.Handler, StateChangeEvent.StateChangeHandler {
 
     private VTextField field;
-    private Label resetButtonLabel;
+    private HTML resetButtonDiv;
 
     @Override
     protected void extend(ServerConnector target) {
         field = (VTextField) ((ComponentConnector) target).getWidget();
         field.addStyleName(VTextField.CLASSNAME + "-withresetbutton");
 
-        resetButtonLabel = Label.wrap(DOM.createDiv());
-        resetButtonLabel.addStyleName(VTextField.CLASSNAME + "-resetbutton");
+        resetButtonDiv = HTML.wrap(DOM.createDiv());
+        resetButtonDiv.addStyleName(VTextField.CLASSNAME + "-resetbutton");
 
         field.addAttachHandler(this);
         field.addKeyUpHandler(this);
 
-        resetButtonLabel.addClickHandler(this);
+        resetButtonDiv.addClickHandler(this);
     }
 
     @Override
     public void onAttachOrDetach(AttachEvent event) {
         if (event.isAttached()) {
             field.getElement().getParentElement()
-                    .appendChild(resetButtonLabel.getElement());
+                    .appendChild(resetButtonDiv.getElement());
             setResetButtonVisibility();
         }
     }
@@ -72,9 +72,9 @@ public class ResetButtonForTextFieldConnector extends
 
     private void setResetButtonVisibility() {
         if (field.getValue().isEmpty()) {
-            resetButtonLabel.setVisible(false);
+            resetButtonDiv.setVisible(false);
         } else {
-            resetButtonLabel.setVisible(true);
+            resetButtonDiv.setVisible(true);
         }
     }
 }
