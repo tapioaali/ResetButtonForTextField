80220355383bdcd3fc2e12a4e8d746121a1009f2
Dave Ball <dave@glark.co.uk>
2015-03-02 14:41:26 +0000

    revert unneccesary formatting changes...

--- src/org/vaadin/resetbuttonfortextfield/ResetButtonForTextFieldTestUI.java
+++ src/org/vaadin/resetbuttonfortextfield/ResetButtonForTextFieldTestUI.java
@@ -17,89 +17,102 @@ import com.vaadin.ui.UI;
 @SuppressWarnings("serial")
 public class ResetButtonForTextFieldTestUI extends UI {
 
-	@Override
-	protected void init(VaadinRequest request) {
-		final FormLayout layout = new FormLayout();
-		layout.setMargin(true);
-		setContent(layout);
+    @Override
+    protected void init(VaadinRequest request) {
+        final FormLayout layout = new FormLayout();
+        layout.setMargin(true);
+        setContent(layout);
 
-		final TextField tf = new TextField("Type something, please");
-		tf.setInputPrompt("Some text here");
+        final TextField tf = new TextField("Type something, please");
+        tf.setInputPrompt("Some text here");
 
-		final ResetButtonForTextField resetButton = ResetButtonForTextField.extend(tf);
+        final ResetButtonForTextField resetButton = ResetButtonForTextField
+                .extend(tf);
 
-		layout.addComponent(tf);
-		tf.setImmediate(true);
-		tf.setWidth("300px");
+        layout.addComponent(tf);
+        tf.setImmediate(true);
+        tf.setWidth("300px");
 
-		final Label textChangeEventListenerLabel = new Label();
-		textChangeEventListenerLabel.setHeight("20px");
-		textChangeEventListenerLabel.setCaption("I show text changes!");
-		layout.addComponent(textChangeEventListenerLabel);
+        final Label textChangeEventListenerLabel = new Label();
+        textChangeEventListenerLabel.setHeight("20px");
+        textChangeEventListenerLabel.setCaption("I show text changes!");
+        layout.addComponent(textChangeEventListenerLabel);
 
-		final Label valueChangeListenerLabel = new Label();
-		valueChangeListenerLabel.setCaption("I show value changes!");
-		layout.addComponent(valueChangeListenerLabel);
-		valueChangeListenerLabel.setHeight("20px");
+        final Label valueChangeListenerLabel = new Label();
+        valueChangeListenerLabel.setCaption("I show value changes!");
+        layout.addComponent(valueChangeListenerLabel);
+        valueChangeListenerLabel.setHeight("20px");
 
-		final Label resetButtonClickListenerLabel = new Label();
-		resetButtonClickListenerLabel.setCaption("I notice reset button clicks!");
-		layout.addComponent(resetButtonClickListenerLabel);
-		resetButtonClickListenerLabel.setHeight("20px");
+        final Label resetButtonClickListenerLabel = new Label();
+        resetButtonClickListenerLabel
+                .setCaption("I notice reset button clicks!");
+        layout.addComponent(resetButtonClickListenerLabel);
+        resetButtonClickListenerLabel.setHeight("20px");
 
-		tf.addValueChangeListener(new Property.ValueChangeListener() {
-			@Override
-			public void valueChange(ValueChangeEvent event) {
-				valueChangeListenerLabel.setValue(event.getProperty().getValue().toString());
-			}
-		});
+        tf.addValueChangeListener(new Property.ValueChangeListener() {
+            @Override
+            public void valueChange(ValueChangeEvent event) {
+                valueChangeListenerLabel.setValue(event.getProperty()
+                        .getValue().toString());
+            }
+        });
 
-		tf.addTextChangeListener(new FieldEvents.TextChangeListener() {
-			@Override
-			public void textChange(TextChangeEvent event) {
-				textChangeEventListenerLabel.setValue(event.getText());
-			}
-		});
+        tf.addTextChangeListener(new FieldEvents.TextChangeListener() {
+            @Override
+            public void textChange(TextChangeEvent event) {
+                textChangeEventListenerLabel.setValue(event.getText());
+            }
+        });
 
-		resetButton.addResetButtonClickedListener(new ResetButtonClickListener() {
-			private int clickCount = 0;
+        resetButton
+                .addResetButtonClickedListener(new ResetButtonClickListener() {
+                    private int clickCount = 0;
 
-			@Override
-			public void resetButtonClicked() {
-				clickCount++;
-				resetButtonClickListenerLabel.setValue("Reset button clicked " + clickCount + " times");
-			}
-		});
+                    @Override
+                    public void resetButtonClicked() {
+                        clickCount++;
+                        resetButtonClickListenerLabel
+                                .setValue("Reset button clicked " + clickCount
+                                        + " times");
+                    }
+                });
 
-		Button b1 = new Button("Clear the value of the textfield", new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				tf.setValue("");
-			}
-		});
-		Button b2 = new Button("Set the value of the textfield to current date", new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				tf.setValue(new Date().toString());
-			}
-		});
-		Button b3 = new Button("Toggle Enabled", new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				tf.setEnabled(!tf.isEnabled());
-			}
-		});
-		Button b4 = new Button("Toggle ReadOnly", new Button.ClickListener() {
-			@Override
-			public void buttonClick(ClickEvent event) {
-				tf.setReadOnly(!tf.isReadOnly());
-			}
-		});
+        Button b1 = new Button("Clear the value of the textfield",
+                new Button.ClickListener() {
+                    @Override
+                    public void buttonClick(ClickEvent event) {
+                        tf.setValue("");
+                    }
+                });
+        Button b2 = new Button(
+                "Set the value of the textfield to current date",
+                new Button.ClickListener() {
+                    @Override
+                    public void buttonClick(ClickEvent event) {
+                        tf.setValue(new Date().toString());
+                    }
+                });
+        Button b3 = new Button(
+                "Toggle Enabled", 
+                new Button.ClickListener() {
+                    @Override
+                    public void buttonClick(ClickEvent event) {
+                        tf.setEnabled(!tf.isEnabled());
+                    }
+                });
+        Button b4 = new Button(
+                "Toggle Read Only", 
+                new Button.ClickListener() {
+                    @Override
+                    public void buttonClick(ClickEvent event) {
+                        tf.setReadOnly(!tf.isReadOnly());
+                    }
+                });
 
-		layout.addComponent(b1);
-		layout.addComponent(b2);
-		layout.addComponent(b3);
-		layout.addComponent(b4);
-	}
+        layout.addComponent(b1);
+        layout.addComponent(b2);
+        layout.addComponent(b3);
+        layout.addComponent(b4);
+    }
 
 }
--- src/org/vaadin/resetbuttonfortextfield/widgetset/client/ResetButtonForTextFieldConnector.java
+++ src/org/vaadin/resetbuttonfortextfield/widgetset/client/ResetButtonForTextFieldConnector.java
@@ -19,89 +19,93 @@ import com.vaadin.client.ui.VTextField;
 import com.vaadin.shared.ui.Connect;
 
 @Connect(ResetButtonForTextField.class)
-public class ResetButtonForTextFieldConnector extends AbstractExtensionConnector implements KeyUpHandler,
-		AttachEvent.Handler, StateChangeEvent.StateChangeHandler {
-	private static final long serialVersionUID = -737765038361894693L;
+public class ResetButtonForTextFieldConnector extends
+        AbstractExtensionConnector implements KeyUpHandler,
+        AttachEvent.Handler, StateChangeEvent.StateChangeHandler {
+    private static final long serialVersionUID = -737765038361894693L;
 
-	public static final String CLASSNAME = "resetbuttonfortextfield";
+    public static final String CLASSNAME = "resetbuttonfortextfield";
 
-	private VTextField textField;
-	private Element resetButtonElement;
-	private ResetButtonClickRpc resetButtonClickRpc = RpcProxy.create(ResetButtonClickRpc.class, this);
+    private VTextField textField;
+    private Element resetButtonElement;
+    private ResetButtonClickRpc resetButtonClickRpc = RpcProxy.create(
+            ResetButtonClickRpc.class, this);
 
-	@Override
-	protected void extend(ServerConnector target) {
-		target.addStateChangeHandler(new StateChangeEvent.StateChangeHandler() {
-			private static final long serialVersionUID = -8439729365677484553L;
+    @Override
+    protected void extend(ServerConnector target) {
+        target.addStateChangeHandler(new StateChangeEvent.StateChangeHandler() {
+            private static final long serialVersionUID = -8439729365677484553L;
 
-			@Override
-			public void onStateChanged(StateChangeEvent stateChangeEvent) {
-				Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-					@Override
-					public void execute() {
-						updateResetButtonVisibility();
-					}
-				});
-			}
-		});
+            @Override
+            public void onStateChanged(StateChangeEvent stateChangeEvent) {
+                Scheduler.get().scheduleDeferred(new ScheduledCommand() {
+                    @Override
+                    public void execute() {
+                        updateResetButtonVisibility();
+                    }
+                });
+            }
+        });
 
-		textField = (VTextField) ((ComponentConnector) target).getWidget();
-		textField.addStyleName(CLASSNAME + "-textfield");
+        textField = (VTextField) ((ComponentConnector) target).getWidget();
+        textField.addStyleName(CLASSNAME + "-textfield");
 
-		resetButtonElement = DOM.createDiv();
-		resetButtonElement.addClassName(CLASSNAME + "-resetbutton");
+        resetButtonElement = DOM.createDiv();
+        resetButtonElement.addClassName(CLASSNAME + "-resetbutton");
 
-		textField.addAttachHandler(this);
-		textField.addKeyUpHandler(this);
-	}
+        textField.addAttachHandler(this);
+        textField.addKeyUpHandler(this);
+    }
 
-	public native void addResetButtonClickListener(Element el)
-	/*-{
-	    var self = this; 
-	    el.onclick = $entry(function() { 
-	        self.@org.vaadin.resetbuttonfortextfield.widgetset.client.ResetButtonForTextFieldConnector::clearTextField()();
-	    }); 
-	}-*/;
+    public native void addResetButtonClickListener(Element el)
+    /*-{
+        var self = this; 
+        el.onclick = $entry(function() { 
+            self.@org.vaadin.resetbuttonfortextfield.widgetset.client.ResetButtonForTextFieldConnector::clearTextField()();
+        }); 
+    }-*/;
 
-	public native void removeResetButtonClickListener(Element el)
-	/*-{
-	    el.onclick = null;
-	}-*/;
+    public native void removeResetButtonClickListener(Element el)
+    /*-{
+        el.onclick = null;
+    }-*/;
 
-	@Override
-	public void onAttachOrDetach(AttachEvent event) {
-		if (event.isAttached()) {
-			textField.getElement().getParentElement().insertAfter(resetButtonElement, textField.getElement());
-			updateResetButtonVisibility();
-			addResetButtonClickListener(resetButtonElement);
-		} else {
-			Element parentElement = resetButtonElement.getParentElement();
-			if (parentElement != null) {
-				parentElement.removeChild(resetButtonElement);
-			}
-			removeResetButtonClickListener(resetButtonElement);
-		}
-	}
+    @Override
+    public void onAttachOrDetach(AttachEvent event) {
+        if (event.isAttached()) {
+            textField.getElement().getParentElement()
+                    .insertAfter(resetButtonElement, textField.getElement());
+            updateResetButtonVisibility();
+            addResetButtonClickListener(resetButtonElement);
+        } else {
+            Element parentElement = resetButtonElement.getParentElement();
+            if (parentElement != null) {
+                parentElement.removeChild(resetButtonElement);
+            }
+            removeResetButtonClickListener(resetButtonElement);
+        }
+    }
 
-	@Override
-	public void onKeyUp(KeyUpEvent event) {
-		updateResetButtonVisibility();
-	}
+    @Override
+    public void onKeyUp(KeyUpEvent event) {
+        updateResetButtonVisibility();
+    }
 
-	private void updateResetButtonVisibility() {
-		if (textField.getValue().isEmpty() || textField.isReadOnly() || !textField.isEnabled()
-				|| textField.getStyleName().contains("v-textfield-prompt")) {
-			resetButtonElement.getStyle().setDisplay(Display.NONE);
-		} else {
-			resetButtonElement.getStyle().clearDisplay();
-		}
-	}
+    private void updateResetButtonVisibility() {
+        if (textField.getValue().isEmpty()
+        		|| textField.isReadOnly() || !textField.isEnabled()
+                || textField.getStyleName().contains("v-textfield-prompt")) {
+            resetButtonElement.getStyle().setDisplay(Display.NONE);
+        } else {
+            resetButtonElement.getStyle().clearDisplay();
+        }
+    }
 
-	private void clearTextField() {
-		resetButtonClickRpc.resetButtonClick();
-		textField.setValue("");
-		textField.valueChange(true);
-		updateResetButtonVisibility();
-		textField.getElement().focus();
-	}
+    private void clearTextField() {
+        resetButtonClickRpc.resetButtonClick();
+        textField.setValue("");
+        textField.valueChange(true);
+        updateResetButtonVisibility();
+        textField.getElement().focus();
+    }
 }
